ee503bdd5ddc00a3c1a3b90110b22967ae1ee7d5
Gaurav <gmpaliwal21@gmail.com>
2024-01-20 01:59:29 +0530

    Connected-Wallet

--- App.js
+++ App.js
@@ -1,19 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import '@walletconnect/react-native-compat'
+import { WagmiConfig } from 'wagmi'
+import { StyleSheet, Text, View } from 'react-native'
+
+import { mainnet, polygon, arbitrum } from 'viem/chains'
+import { createWeb3Modal, defaultWagmiConfig, Web3Modal } from '@web3modal/wagmi-react-native'
+import ConnectView from './src/components/ConnectWalletView'
+const projectId = 'd8dab80ccbbfdcc48f5f3870d0df4e1d'
+
+const metadata = {
+  name: 'Guard Anon RN',
+  description: 'Crime Prevention Tip App',
+  url: 'https://web3modal.com',
+  icons: ['https://avatars.githubusercontent.com/u/37784886'],
+  redirect: {
+    native: 'YOUR_APP_SCHEME://',
+    universal: 'YOUR_APP_UNIVERSAL_LINK.com'
+  }
 }
 
+const chains = [mainnet, polygon, arbitrum]
+
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
+
+// 3. Create modal
+createWeb3Modal({
+  projectId,
+  chains,
+  wagmiConfig
+})
+
+export default function App() {
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <View style = {styles.container}>
+      <ConnectView />
+      </View>
+      <Web3Modal />
+    </WagmiConfig>
+  )
+}
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  container : {
+    flex : 1,
+    alignItems : 'center',
+    justifyContent : 'center'
+  }
+})
